895f5cc3e3c655971ec6885b7f515c424e42ba82
Yoji Tsukada <ytsukada@ytsukada-mbp.local>
2018-11-24 00:00:53 +0900

    リストが返るに設定

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # rh_security
+
+Check Redhat Security Tools.
+
+### Requires
+```
+# requests: HTTP ライブラリ
+pip install requests
+
+# beautifulsoup4: htmlパーサー(pythonから呼び出し)　（不要)
+pip install beautifulsoup4
+
+# lxml: htmlパーサー(beautifulsoup4内部で利用) （不要)
+pip install lxml
+
+# Web Driverをインストール　（不要)
+pip install selenium
+```
+
+
+
+
---
+++ lambda/parse/lambda_function.py
@@ -0,0 +1,59 @@
+import os
+import requests
+import json
+import base64
+
+
+def lambda_handler(event, context):
+    
+    # Redhat Security API URL
+    base_url = 'https://access.redhat.com/labs/securitydataapi/'
+    
+    # Parameter
+    endpoint = 'cvrf.json?'
+    date = "after=2018-11-10"
+    severity = 'severity=important'
+    
+    # Build URL
+    url = base_url + endpoint + date + "&" + "&" + severity
+    #data = get_data(endpoint + '?' + params)
+
+    res = requests.get(url)
+    data = res.json()
+    
+    # To check Response 
+    if data == []:
+        print('No updated.')
+        return None
+
+    # initalize for lists
+    sec_list = []
+    tmp_list = []
+
+    for x in data:
+        detail = requests.get(x['resource_url'] ,timeout=10)
+
+        # To check Kenel updata:
+        package = x['released_packages']
+
+        if 'kernel' in package:
+            print('This is a kernel update.')
+
+        if detail.json()['cvrfdoc']['document_type'] == 'Security Advisory':
+            
+            # build temporary list.
+            tmp_list.append(x['RHSA'])
+            tmp_list.append(detail.json()['cvrfdoc']['document_title'])
+            tmp_list.append(detail.json()['cvrfdoc']['document_notes']['note'])
+            tmp_list.append(detail.json()['cvrfdoc']['document_references']['reference'][0]['url'])
+            tmp_list.append(x['released_on']) 
+            tmp_list.append(x['CVEs']) 
+
+            # merge.
+            sec_list.append(tmp_list)
+            tmp_list = []
+
+        else:
+            print('Not Security Severity.')
+
+    return json.dumps(sec_list)
